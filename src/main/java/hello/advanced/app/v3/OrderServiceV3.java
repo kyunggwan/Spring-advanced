5e929178f6d3bde382567afed1a5e79951acf69a
kyunggwan <4562sky@naver.com>
2024-11-08 23:37:01 +0900

    feat: 필드 동기화 적용

---
+++ src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception ex) {
+            trace.exception(status, ex);
+            throw ex; // 로그 때문에 정상 흐름이 방해되면 안되기 때문에 예외를 다시 던짐
+        }
+
+    }
+}
